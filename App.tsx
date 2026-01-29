eb7ab8f7935bd1c22d46fde51948b7b912d76a2a
nenadjesic <master@nenadjesic.com>
2026-01-29 17:24:23 +0100

    css of list

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 
-import 'react-native-gesture-handler'; // Prva linija!
+import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
